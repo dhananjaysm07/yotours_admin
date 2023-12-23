ef314e1d56db62cb8a89df4206c769a987d2ac1e
dhananjay <dhananjaysm0712@gmail.com>
2023-12-23 18:15:56 +0530

    changes

--- src/pages/thing/CreateThingPage.tsx
+++ src/pages/thing/CreateThingPage.tsx
@@ -237,7 +237,6 @@ const CreateThingPage = () => {
               value={thingHyperlink}
               onChange={(e) => setThingHyperlink(e.target.value)}
               className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-              required
             />
           </div>
           <div className="mb-4">
